77608695d73f7fae94986b3df61c89df202e6301
IsraMejia <ihmaisrael@gmail.com>
2022-09-27 16:15:48 -0500

    Commit del hola mundo

---
+++ src/HolaMundo.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	//fmt  paquete  que se encarga de mostrar salidas en consola
+	fmt.Println("Hello World, Hola mundo GO")
+	fmt.Println("Salu2 rex \t Salu2 coco")
+}
